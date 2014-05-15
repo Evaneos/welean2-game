0c55c58aea2cc277358e4cc35bd7f0de3ff7ee5e
Christophe Hurpeau <christophe@evaneos.com>
2014-05-16 00:31:15 +0200

    server

--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,25 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-
-app.use(require('connect-livereload')());
-
-
-app.get('/', function(req, res) {
-    res.render('index');
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        'production': 'prod'
+    }
 });
+
+if (!argv.production) {
+    console.log('Dev mode');
+    app.use(require('connect-livereload')({
+        port: argv.livereloadPort
+    }));
+    ['src', 'node_modules'].forEach((folder) => {
+        app.use('/' + folder, express.static(__dirname +'/../../' + folder));
+    });
+} else {
+    console.log('Production mode');
+}
+
+app.get('*', (req, res) => res.render('index', { basepath: argv.basepath || '/', URL: req.path }));
 
 app.use(express.static(__dirname +'/../../public'));
 
