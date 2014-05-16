88c49abd60d2f6ca1ac74731ed84083ca7556dcd
remi <remi@evaneos.com>
2014-05-16 05:39:39 +0200

    Updated application factory

--- src/server/factories/ApplicationFactory.js
+++ src/server/factories/ApplicationFactory.js
@@ -3,10 +3,17 @@ var Bataille = require('../models/game/cardGame/bataille/Bataille');
 var ApplicationFactory = S.newClass();
 module.exports = ApplicationFactory;
 
+ApplicationFactory.defineProperty("applications", { BATAILLE : "bataille"});
+
 ApplicationFactory.extendPrototype({
 	construct() {
     },
     get(key, socket, token) {
-    	return new Bataille(socket, token);
+    	switch(key) {
+    		case ApplicationFactory.applications.BATAILLE :
+    			return new Bataille(socket, token);
+    		default :
+    			throw new Error("application.unknown");
+    	}
     }
 });
