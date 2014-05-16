5473f29a874190b4a5138718e5eceaaad35f9797
Christophe Hurpeau <christophe@evaneos.com>
2014-05-16 09:17:10 +0200

    ApplicationFactory

--- src/server/controllers/roomcreate.js
+++ src/server/controllers/roomcreate.js
@@ -1,6 +1,6 @@
 var token2app = require('../token.js');
 var generator = require('springbokjs-utils/generator');
-var applicationFactory = require('../factories/ApplicationFactory.js');
+var ApplicationFactory = require('../factories/ApplicationFactory.js');
 
 module.exports = function(req, res) {
     // res.render('smartphone', { myData : "myValue" });
@@ -21,7 +21,7 @@ module.exports = function(req, res) {
     }
 
     // Create an Application with a Room, ready to receive sockets
-    var app = new applicationFactory().get(gameKey, null, token);
+    var app = new ApplicationFactory().get(gameKey, null, token);
 
     token2app[token] = app;
     res.render(gameKey + '/roomboard/index', { token : token });
