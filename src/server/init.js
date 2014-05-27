0faa31282105a1674d9ce1804226b5186fec88f9
Christophe Hurpeau <christophe@evaneos.com>
2014-05-27 23:24:36 +0200

    init

---
+++ src/server/init.js
@@ -0,0 +1,4 @@
+require('springbokjs-shim/es6');
+global.S = require('springbokjs-utils');
+var ConsoleLogger = require('springbokjs-logger/console');
+global.logger = new ConsoleLogger();
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,4 @@
-require('springbokjs-shim/es6');
-global.S = require('springbokjs-utils');
-var ConsoleLogger = require('springbokjs-logger/console');
-global.logger = new ConsoleLogger();
-
+require('./init');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
